786f69750467b018fd69ad83ea2eeb4ee40ce9d5
Ryota Kameoka <ok@ryota-ka.me>
2022-04-03 00:32:42 +0900

    Turn on `@typescript-eslint/no-unused-expressions`

--- base/CHANGELOG.md
+++ base/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change log
 
+## v0.16.0
+
+### Breaking changes
+
+- Turn on [`@typescript-eslint/no-unused-expressions`](https://typescript-eslint.io/rules/no-unused-expressions/).
+
 ## v0.15.0
 
 ### Breaking changes
--- base/index.js
+++ base/index.js
@@ -112,6 +112,7 @@ module.exports = {
     '@typescript-eslint/no-require-imports': 'error',
     '@typescript-eslint/no-throw-literal': 'error',
     '@typescript-eslint/no-unnecessary-type-constraint': 'off',
+    '@typescript-eslint/no-unused-expressions': ['error', { enforceForJSX: true }],
     '@typescript-eslint/no-unused-vars': 'off', // Let TypeScript check it
     '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
     '@typescript-eslint/no-useless-constructor': 'error',
