d918ebaef43d706489f9d69eba5a94fb6a76fd17
Ryota Kameoka <ok@ryota-ka.me>
2022-11-08 20:37:35 +0900

    Turn off `react/jsx-no-target-blank`
    https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-target-blank.md

--- react/CHANGELOG.md
+++ react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change log
 
+## v0.3.0
+
+### Non-breaking changes
+
+- Turn off [`react/jsx-no-target-blank`](https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-target-blank.md).
+
 ## v0.2.0
 
 ### Breaking changes
--- react/index.js
+++ react/index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
   plugins: ['react', 'react-hooks'],
   rules: {
+    'react/jsx-no-target-blank': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
   },
