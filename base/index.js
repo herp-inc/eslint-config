6dbbcef89d2dd0821bb395a430824347a8871101
Ryota Kameoka <ok@ryota-ka.me>
2024-06-17 22:26:06 +0900

    Enable `import/exports-last` as a preview

--- base/index.js
+++ base/index.js
@@ -199,6 +199,7 @@ module.exports = {
         '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'warn',
         '@typescript-eslint/no-unnecessary-template-expression': 'warn',
         'import/consistent-type-specifier-style': ['warn', 'prefer-top-level'],
+        'import/exports-last': 'warn',
       },
     },
     {
